41e82788ddbdba10c481157d34fadf293f557a8d
Omar Abumazen <omarabumazen990@gmail.com>
2026-03-18 03:44:04 +0200

    try to fix again and seem I find a good soltuion but not the perfect one!

--- DSA/heapsort/MAX-HEAPIFY/version1.cpp
+++ DSA/heapsort/MAX-HEAPIFY/version1.cpp
@@ -5,6 +5,11 @@
 using namespace std;
 
 
+/// Mar 18, 2026 - 3:01 am, you're good is work when you add a new value less than the root, if the value of the root is larger, the biggest value will alaways be in the right node of the root node.
+
+/// Mar 18, 2026 - 3:06 - I fix the code by when i finished the code, swap to be sure that we make the right heap... I think, but I'm not sure yet hahahaha
+
+
 vector<int> readNumbersFromFile(const string& filename)
 {
     ifstream file(filename);
@@ -37,14 +42,16 @@ vector<int> heapify(vector <int> A){
 		int right = left + 1; // + 2 with inital 2
 		int parent = floor((i-1)/2); // with inital -1   +0.5
 
-
-
 		if(left < A.size() && A[left] > A[i]){ swap(A[i],A[left]); }
 
 		if(right < A.size() && A[right] > A[i]){ swap(A[i],A[right]); }
 
 		if(parent >= 0 && A[i] > A[parent]){ swap(A[i],A[parent]); }
+
 	}
+
+	if(A[0] < A[2]){swap(A[0],A[2]);}
+	else if(A[0] < A[1]){swap(A[0],A[1]);}
 
 	return A;
 
@@ -90,9 +97,25 @@ int main()
 
 	//vector<int> arr = {10, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-	vector<int> arr = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
+	//vector<int> arr = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
 
 	//vector<int> arr = readNumbersFromFile("numbers.txt");
+
+	//vector <int> arr = {1, 10, 9, 8, 7, 6, 5};
+
+	//vector<int> arr = {35, 1, 3, 2, 16, 9, 10, 14, 8, 7};
+
+	//vector<int> arr = {5, 4, 3, 2, 1, 100};
+
+	//vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
+
+	//vector<int> arr = {10, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	//vector<int> arr = {3, 17, 2, 1, 25, 7, 36, 19, 100};
+
+	//vector<int>  arr = {5, 12, 11, 13, 4, 6, 7, 8, 9, 10};
+
+	vector<int>  arr = {2, 50, 3, 40, 45, 1, 4, 39, 38, 37, 36};
 
 
 
@@ -106,6 +129,8 @@ int main()
 
 
 	printArray(arr);
+
+return 0;
 
 	int i = 0;
 
