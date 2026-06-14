547d6f66a46681d24c8247d448ca789202de69f1
Omar Abumazen <omarabumazen990@gmail.com>
2026-06-14 20:36:51 +0300

    v3 - full binary tree - order by level print tree

---
+++ DSA/binaryTrees/fullBinaryTree/fullBinaryTree-v3.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct node{
+		int value;
+		char sy;
+		node *left;
+		node *right;
+
+		node(int x){
+				value = x;
+				left = NULL;
+				right = NULL;
+		}
+
+		node(char x){
+				sy = x;
+				left = NULL;
+				right = NULL;
+		}
+};
+
+
+void insertNode(node *root, int value){
+
+	node *new_node = new node(value);
+
+	if(root == NULL){ root = new_node; return;};
+
+	node *current,*trailCurrent;
+
+	current = root;
+
+	while(current != NULL){
+
+		trailCurrent = current;
+
+		if(current->value == value) cerr << "Sorry we can't duplicate values"<<endl;
+
+		else if(current-> value > value) current = current->left;
+
+		else current = current->right;
+	}
+
+		if(trailCurrent->value < value){
+
+			trailCurrent->right = new_node;
+		}
+
+		else trailCurrent->left = new_node;
+}
+
+
+void createFullBinaryTree(node *root,vector<int> &arr){
+	
+	for(int i=0; i < arr.size();i++){		
+		insertNode(root,arr[i]);
+	}
+}
+
+
+void orderByLevel(node *root){
+
+	if (!root)
+        return;
+
+    queue<node*> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        node* newNode = q.front();
+        q.pop();
+
+        cout << newNode->value << ", ";
+
+        if (newNode->left)
+            q.push(newNode->left);
+
+        if (newNode->right)
+            q.push(newNode->right);
+    }
+
+cout << endl;
+
+}
+
+
+
+
+
+
+
+int main()
+{
+        vector<int> A = {2,4,6,8,10,12};
+
+	node *root = new node(1);
+
+	createFullBinaryTree(root,A);
+
+	orderByLevel(root);
+
+	insertNode(root,5);
+
+		orderByLevel(root);
+	
+	
+
+	insertNode(root,7);
+
+		orderByLevel(root);
+
+        
+
+
+        return 0;
+}
