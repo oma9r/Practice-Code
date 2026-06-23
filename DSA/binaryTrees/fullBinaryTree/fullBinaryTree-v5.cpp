bf28f1355953775725765b1fc7bb7aa54708e45f
Omar Abumazen <omarabumazen990@gmail.com>
2026-06-23 10:18:56 +0300

    finish full binary tree! :)

--- DSA/binaryTrees/fullBinaryTree/fullBinaryTree-v5.cpp
+++ DSA/binaryTrees/fullBinaryTree/fullBinaryTree-v5.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <bits/stdc++.h>
+#include <cmath>
 using namespace std;
 
 struct node{
@@ -53,12 +55,7 @@ void insertNode(node *root, int value){
 }
 
 
-void createFullBinaryTree(node *root,vector<int> &arr){
-	
-	for(int i=0; i < arr.size();i++){		
-		insertNode(root,arr[i]);
-	}
-}
+
 
 
 void orderByLevel(node *root){
@@ -93,63 +90,117 @@ void insert(node *root, int value){
 	if(root == NULL) root = new node(value);
 
 	else{
-		node *current = root;
-		
-		if(current->left == NULL) current->left = new node(value);
+		queue<node *> q;
+		q.push(root);
 
-		else if(current->right == NULL) current->right = new node(value);
+		node *current;
 
-		else{
-			queue<node *> q;
-			current = current->left;
-			q.push(current);
+		while(!q.empty()){
+
+			current = q.front();
+			q.pop();
+
+			if(current->left == NULL){
+
+				current->left = new node(value);
+				break;
+			}						
 			
+			else if(current->right == NULL){
 			
+				current->right = new node(value);
+				break;
+			}
 
+			else{
+				q.push(current->left);
+				q.push(current->right);
+			}
 
 		}
-
-
-
-
-
-
-
 	}
+}
 
 
+void createFullBinaryTree(node *root,vector<int> &arr){
+	
+	for(int i=0; i < arr.size();i++){		
+		insert(root,arr[i]);
+	}
+}
 
 
+bool checkFullBinaryTree(node *root){
 
+	if(root->left != NULL && root->right != NULL) return true;
+
+	else if(root->left == NULL && root->right == NULL) return true;
+	
+	else if( (root->left != NULL && root->right == NULL) || (root->left == NULL && root->right != NULL) ) return false; 
+
+	else return checkFullBinaryTree(root->left) && checkFullBinaryTree(root->right);
+}
+
+
+bool checkFullBinaryTree(int countNode){
+
+	double value = (double)log2(countNode);
+	//cout << "the value of log2 = " << value << endl;
+
+	return (floor(value) == value); 
 }
 
 
 
+int countNodes(node *root){
 
+	if(root == NULL) return 0;
 
+	else{
+		return 1 + countNodes(root->left) + countNodes(root->right);
+		
+	}
+};
 
 
 
 
 int main()
 {
-        //vector<int> A = {2,4,6,8,10,12};
+        vector<int> A = {4,6,8,10,12};
 
-	vector<int> A = {55,98,33,60,80,102,10,41,56,65,79,85,100,120,7,21,39,40,58,59};
+	//vector<int> A = {55,98,33,60,80,102,10,41,56,65,79,85,100,120,7,21,39,40,58,59};
 
-	node *root = new node(76);
+	//node *root = new node(76);
+
+	node *root = new node(2);
 
 	createFullBinaryTree(root,A);
 
 	orderByLevel(root);
 
-	//insertNode(root,5);
+	int size = countNodes(root) + 1;
+
+	//cout << "size = " << size - 1 << endl;
+
+	if(checkFullBinaryTree(size)) cout << "this is a full binary tree" << endl;
+
+	if(checkFullBinaryTree(root)) cout << "This is also full binary tree!" << endl;
+
+
+	cout << endl << "before anything" << endl;
+	
+	insert(root,5);
 
 	//orderByLevel(root);
 
-	//insertNode(root,7);
+	//insert(root,7);
 
-	//orderByLevel(root);
+	orderByLevel(root);
+
+	if(checkFullBinaryTree(countNodes(root) + 1)) cout << "this is a full binary tree" << endl;
+
+	if(checkFullBinaryTree(root)) cout << "This is also full binary tree!" << endl;
 
 	//deleteNode(root,33);
 
